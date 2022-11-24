2d79e603048bb03acf4984387b697804447bfcff
shubham <shubhamhatagale02@gmail.com>
2022-11-24 10:07:48 +0530

    login_redirection24Nov2022

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -103,7 +103,7 @@ export const Login = () => {
                         localStorage.setItem("masters_id", resData.resultSet.userId)
                         localStorage.setItem("company", JSON.stringify(resData.resultSet))
                         // dispatch({ type: "COMPANY", payload: resData.resultSet })
-                        history.push("/company_profile")
+                        history.push("/password_setting")
                         M.toast({
                             html: "login Successful",
                             classes: "#e#00e676 green accent-3",
